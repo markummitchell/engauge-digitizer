a73c9a51869e4aa94f0d3d34c4df977e36f7ef9a
Mark Mitchell <markummitchell@gmail.com>
2014-12-10 23:29:01 -0800

    Filter parameters

--- src/Document/DocumentModelFilter.cpp
+++ src/Document/DocumentModelFilter.cpp
@@ -4,20 +4,48 @@
 #include <QXmlStreamWriter>
 
 DocumentModelFilter::DocumentModelFilter() :
-  m_filterParameter (FILTER_PARAMETER_INTENSITY)
+  m_filterParameter (FILTER_PARAMETER_INTENSITY),
+  m_intensityLow (INTENSITY_LOW_DEFAULT),
+  m_intensityHigh (INTENSITY_HIGH_DEFAULT),
+  m_foregroundLow (FOREGROUND_LOW_DEFAULT),
+  m_foregroundHigh (FOREGROUND_HIGH_DEFAULT),
+  m_hueLow (HUE_LOW_DEFAULT),
+  m_hueHigh (HUE_HIGH_DEFAULT),
+  m_saturationLow (SATURATION_LOW_DEFAULT),
+  m_saturationHigh (SATURATION_HIGH_DEFAULT),
+  m_valueLow (VALUE_LOW_DEFAULT),
+  m_valueHigh (VALUE_HIGH_DEFAULT)
 {
 }
 
 DocumentModelFilter::DocumentModelFilter(const Document &document) :
-  m_filterParameter (document.modelFilter().filterParameter())
+  m_filterParameter (document.modelFilter().filterParameter()),
+  m_intensityLow (document.modelFilter().intensityLow()),
+  m_intensityHigh (document.modelFilter().intensityHigh()),
+  m_foregroundLow (document.modelFilter().foregroundLow()),
+  m_foregroundHigh (document.modelFilter().foregroundHigh()),
+  m_hueLow (document.modelFilter().hueLow()),
+  m_hueHigh (document.modelFilter().hueHigh()),
+  m_saturationLow (document.modelFilter().saturationLow()),
+  m_saturationHigh (document.modelFilter().saturationHigh()),
+  m_valueLow (document.modelFilter().valueLow()),
+  m_valueHigh (document.modelFilter().valueHigh())
 {
-
 }
 
 DocumentModelFilter::DocumentModelFilter(const DocumentModelFilter &other) :
-  m_filterParameter (other.filterParameter())
+  m_filterParameter (other.filterParameter()),
+  m_intensityLow (other.intensityLow()),
+  m_intensityHigh (other.intensityHigh()),
+  m_foregroundLow (other.foregroundLow()),
+  m_foregroundHigh (other.foregroundHigh()),
+  m_hueLow (other.hueLow()),
+  m_hueHigh (other.hueHigh()),
+  m_saturationLow (other.saturationLow()),
+  m_saturationHigh (other.saturationHigh()),
+  m_valueLow (other.valueLow()),
+  m_valueHigh (other.valueHigh())
 {
-
 }
 
 DocumentModelFilter &DocumentModelFilter::operator=(const DocumentModelFilter &other)
@@ -32,6 +60,46 @@ FilterParameter DocumentModelFilter::filterParameter() const
   return m_filterParameter;
 }
 
+int DocumentModelFilter::foregroundHigh () const
+{
+  return m_foregroundHigh;
+}
+
+int DocumentModelFilter::foregroundLow () const
+{
+  return m_foregroundLow;
+}
+
+int DocumentModelFilter::hueHigh () const
+{
+  return m_hueHigh;
+}
+
+int DocumentModelFilter::hueLow () const
+{
+  return m_hueLow;
+}
+
+int DocumentModelFilter::intensityHigh () const
+{
+  return m_intensityHigh;
+}
+
+int DocumentModelFilter::intensityLow () const
+{
+  return m_intensityLow;
+}
+
+int DocumentModelFilter::saturationHigh () const
+{
+  return m_saturationHigh;
+}
+
+int DocumentModelFilter::saturationLow () const
+{
+  return m_saturationLow;
+}
+
 void DocumentModelFilter::saveModel(QXmlStreamWriter &stream) const
 {
   LOG4CPP_INFO_S ((*mainCat)) << "DocumentModelFilter::saveModel";
@@ -43,4 +111,74 @@ void DocumentModelFilter::saveModel(QXmlStreamWriter &stream) const
 void DocumentModelFilter::setFilterParameter(FilterParameter filterParameter)
 {
   m_filterParameter = filterParameter;
+}
+
+void DocumentModelFilter::setForegroundHigh (int foregroundHigh)
+{
+  Q_ASSERT (FOREGROUND_MIN <= foregroundHigh && foregroundHigh <= FOREGROUND_MAX);
+  m_foregroundHigh = foregroundHigh;
+}
+
+void DocumentModelFilter::setForegroundLow (int foregroundLow)
+{
+  Q_ASSERT (FOREGROUND_MIN <= foregroundLow && foregroundLow <= FOREGROUND_MAX);
+  m_foregroundLow = foregroundLow;
+}
+
+void DocumentModelFilter::setHueHigh (int hueHigh)
+{
+  Q_ASSERT (HUE_MIN <= hueHigh && hueHigh <= HUE_MAX);
+  m_hueHigh = hueHigh;
+}
+
+void DocumentModelFilter::setHueLow (int hueLow)
+{
+  Q_ASSERT (HUE_MIN <= hueLow && hueLow <= HUE_MAX);
+  m_hueLow = hueLow;
+}
+
+void DocumentModelFilter::setIntensityHigh (int intensityHigh)
+{
+  Q_ASSERT (INTENSITY_MIN <= intensityHigh && intensityHigh <= INTENSITY_MAX);
+  m_intensityHigh = intensityHigh;
+}
+
+void DocumentModelFilter::setIntensityLow (int intensityLow)
+{
+  Q_ASSERT (INTENSITY_MIN <= intensityLow && intensityLow <= INTENSITY_MAX);
+  m_intensityLow = intensityLow;
+}
+
+void DocumentModelFilter::setSaturationHigh (int saturationHigh)
+{
+  Q_ASSERT (SATURATION_MIN <= saturationHigh && saturationHigh <= SATURATION_MAX);
+  m_saturationHigh = saturationHigh;
+}
+
+void DocumentModelFilter::setSaturationLow (int saturationLow)
+{
+  Q_ASSERT (SATURATION_MIN <= saturationLow && saturationLow <= SATURATION_MAX);
+  m_saturationLow = saturationLow;
+}
+
+void DocumentModelFilter::setValueHigh (int valueHigh)
+{
+  Q_ASSERT (VALUE_MIN <= valueHigh && valueHigh <= VALUE_MAX);
+  m_valueHigh = valueHigh;
+}
+
+void DocumentModelFilter::setValueLow (int valueLow)
+{
+  Q_ASSERT (VALUE_MIN <= valueLow && valueLow <= VALUE_MAX);
+  m_valueLow = valueLow;
+}
+
+int DocumentModelFilter::valueHigh () const
+{
+  return m_valueHigh;
+}
+
+int DocumentModelFilter::valueLow () const
+{
+  return m_valueLow;
 }
--- src/Document/DocumentModelFilter.h
+++ src/Document/DocumentModelFilter.h
@@ -4,6 +4,27 @@
 #include "DocumentModelAbstractBase.h"
 #include "FilterParameter.h"
 
+const int INTENSITY_MIN = 0;
+const int INTENSITY_MAX = 100;
+const int INTENSITY_LOW_DEFAULT = 0;
+const int INTENSITY_HIGH_DEFAULT = 50;
+const int FOREGROUND_MIN = 0;
+const int FOREGROUND_MAX = 100;
+const int FOREGROUND_LOW_DEFAULT = 0;
+const int FOREGROUND_HIGH_DEFAULT = 10;
+const int HUE_MIN = 0;
+const int HUE_MAX = 360;
+const int HUE_LOW_DEFAULT = 180;
+const int HUE_HIGH_DEFAULT = 360;
+const int SATURATION_MIN = 0;
+const int SATURATION_MAX = 100;
+const int SATURATION_LOW_DEFAULT = 50;
+const int SATURATION_HIGH_DEFAULT = 100;
+const int VALUE_MIN = 0;
+const int VALUE_MAX = 100;
+const int VALUE_LOW_DEFAULT = 0;
+const int VALUE_HIGH_DEFAULT = 50;
+
 class Document;
 
 /// Model for DlgSettingsFilter and CmdSettingsFilter.
@@ -25,14 +46,84 @@ public:
   /// Get method for filter parameter.
   FilterParameter filterParameter() const;
 
+  /// Get method for foreground higher bound.
+  int foregroundHigh () const;
+
+  /// Get method for foreground lower bound.
+  int foregroundLow () const;
+
+  /// Get method for hue higher bound.
+  int hueHigh () const;
+
+  /// Get method for hue lower bound.
+  int hueLow () const;
+
+  /// Get method for intensity higher bound.
+  int intensityHigh () const;
+
+  /// Get method for intensity lower bound.
+  int intensityLow () const;
+
+  /// Get method for saturation higher bound.
+  int saturationHigh () const;
+
+  /// Get method for saturation lower bound.
+  int saturationLow () const;
+
   virtual void saveModel(QXmlStreamWriter &stream) const;
 
   /// Set method for filter parameter.
   void setFilterParameter(FilterParameter filterParameter);
 
+  /// Set method for foreground higher bound.
+  void setForegroundHigh (int foregroundHigh);
+
+  /// Set method for foreground lower bound.
+  void setForegroundLow (int foregroundLow);
+
+  /// Set method for hue higher bound.
+  void setHueHigh (int hueHigh);
+
+  /// Set method for hue lower bound.
+  void setHueLow (int hueLow);
+
+  /// Set method for intensity higher bound.
+  void setIntensityHigh (int intensityHigh);
+
+  /// Set method for intensity lower bound.
+  void setIntensityLow (int intensityLow);
+
+  /// Set method for saturation high.
+  void setSaturationHigh (int saturationHigh);
+
+  /// Set method for saturation low.
+  void setSaturationLow (int saturationLow);
+
+  /// Set method for value high.
+  void setValueHigh (int valueHigh);
+
+  /// Set method for value low.
+  void setValueLow (int valueLow);
+
+  /// Get method for value high.
+  int valueHigh () const;
+
+  /// Get method for value low.
+  int valueLow () const;
+
 private:
 
   FilterParameter m_filterParameter;
+  int m_intensityLow;
+  int m_intensityHigh;
+  int m_foregroundLow;
+  int m_foregroundHigh;
+  int m_hueLow;
+  int m_hueHigh;
+  int m_saturationLow;
+  int m_saturationHigh;
+  int m_valueLow;
+  int m_valueHigh;
 };
 
 #endif // DOCUMENT_MODEL_FILTER_H
