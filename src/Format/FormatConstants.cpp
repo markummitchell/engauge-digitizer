80bd455289c8ff9800f0bfe266888dcdde8a2404
Mark Mitchell <markummitchell@gmail.com>
2015-08-14 01:56:47 -0700

    More resolution for exporting small dynamic ranges

--- src/Format/FormatConstants.cpp
+++ src/Format/FormatConstants.cpp
@@ -1,4 +1,6 @@
 #include "FormatConstants.h"
 
-/// Default precision for QString::number
-const int DEFAULT_PRECISION = 6;
+/// Default precision for QString::number. Precision is important for cases like
+/// where latitude and longitude range have precisions of 0.00# degree per pixel, as
+/// compared to total values of ###.#### and ##.#### respectively
+const int DEFAULT_PRECISION = 9;
