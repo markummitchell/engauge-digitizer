591a3c1ea108749f3222b1fdebc998d45754b2e9
mark mitchell <markummitchell@gmail.com>
2015-10-29 22:37:06 -0700

    Removed text from icons. Version number is checked when building release. Windows release is labeled as 32 bit

--- src/main/MainWindow.cpp
+++ src/main/MainWindow.cpp
@@ -107,8 +107,6 @@ const QString TSV_FILENAME_EXTENSION ("tsv");
 
 const unsigned int MAX_RECENT_FILE_LIST_SIZE = 8;
 
-const char *VERSION_NUMBER = "6.0";
-
 MainWindow::MainWindow(const QString &errorReportFile,
                        bool isGnuplot,
                        QWidget *parent) :
