176d60e36a40c51ea865f3ef759d22b2d01a36df
Mark Mitchell <markummitchell@gmail.com>
2016-03-03 22:44:24 -0800

    Accept Finnish decimal numbers as axis coordinates with log coordinates for issue #78

--- src/Dlg/DlgValidatorNumber.cpp
+++ src/Dlg/DlgValidatorNumber.cpp
@@ -30,12 +30,12 @@ QValidator::State DlgValidatorNumber::validate (QString &input,
   if (state == QValidator::Acceptable) {
 
     if (m_coordScale == COORD_SCALE_LOG) {
-      if (input.toDouble () < 0.0) {
+      if (m_locale.toDouble (input) < 0.0) {
 
         // Cannot allow negative number
         state = QValidator::Invalid;
 
-      } if (input.toDouble () == 0.0) {
+      } if (m_locale.toDouble (input) == 0.0) {
 
         // Treat as a leading zero, which is legal
         state = QValidator::Intermediate;
