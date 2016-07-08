50eb84d65a6bcd0c6b5a6d48c532fa93974135d5
Mark Mitchell <markummitchell@gmail.com>
2016-07-08 00:26:40 -0700

    Builds release in windows with msvc

--- dev/windows/build_msi_and_zip.bat
+++ dev/windows/build_msi_and_zip.bat
@@ -8,7 +8,10 @@ set QTLIBEXT='.lib'
 set LOG4CPPDLLINK='https://dl.dropboxusercontent.com/u/1147076/log4cpp-1.1.1.zip'
 
 set SCRIPTDIR=%cd%
-set RESULTDIR="%SCRIPTDIR%\engauge-build"
+
+rem Double-double quotes are needed in next line if the directory has a space
+set RESULTDIR=""%SCRIPTDIR%\Engauge Digitizer""
+
 mkdir "%RESULTDIR%"
 
 rem Directory containing engauge.pro
@@ -39,10 +42,10 @@ curl 'https://dl.dropboxusercontent.com/u/1147076/poppler-qt5.zip' -o poppler-qt
 7z x poppler-qt5.zip -aoa
 
 cd "%APPVEYOR_BUILD_FOLDER%"
-set LOG4CPP_HOME="%APPVEYOR_BUILD_FOLDER%\log4cpp-1.1.1"
 set FFTW_HOME="%APPVEYOR_BUILD_FOLDER%\fftw-3.3.4-dll32"
 set POPPLER_INCLUDE="%APPVEYOR_BUILD_FOLDER%\poppler-qt5\include\poppler\qt5"
 set POPPLER_LIB="%APPVEYOR_BUILD_FOLDER%\poppler-qt5"
+lrelease engauge.pro
 qmake engauge.pro "CONFIG+=%ENGAUGE_CONFIG%"
 rem move Makefile Makefile.orig
 rem ps: gc Makefile.orig | %{ $_ -replace '551.lib', %QTLIBEXT% } > Makefile
@@ -55,7 +58,9 @@ for %%I in (%QTDIRS%) do copy %QTDIR%\plugins\%%I\*.dll "%RESULTDIR%\%%I"
 for %%I in (%QTLIBS%) do copy %QTDIR%\bin\%%I.dll "%RESULTDIR%"
 del /S *d.dll
 copy bin\engauge.exe "%RESULTDIR%"
-copy log4cpp-1.1.1\lib\log4cpp.dll "%RESULTDIR%"
+
+copy "%LOG4CPP_HOME%\lib\log4cpp.dll" "%RESULTDIR%"
+
 copy fftw-3.3.4-dll32\lib\libfftw3-3.dll "%RESULTDIR%"
 copy "%APPVEYOR_BUILD_FOLDER%"\poppler-qt5\*.dll "%RESULTDIR%"
 copy LICENSE "%RESULTDIR%"
@@ -65,3 +70,17 @@ move engauge.qch "%RESULTDIR%"\documentation
 move engauge.qhc "%RESULTDIR%"\documentation
 cd ..
 7z a "%RESULTDIR%.7z" "%RESULTDIR%"
+
+echo ***creating msi
+cd "%SCRIPTDIR%"
+findStr "char *VERSION_NUMBER" ..\..\src\util\Version.cpp
+findStr "Version=" engauge.wxs | findStr /v InstallerVersion
+set /p VERSION_NUMBER_CODE="If the version numbers are correct, enter the version number seen above to continue and build releases> "
+
+candle engauge.wxs
+candle WixUI_InstallDir_NoLicense.wxs
+light.exe -ext WixUIExtension -ext WixUtilExtension engauge.wixobj WixUI_InstallDir_NoLicense.wixobj -o digit-exe-windows-32-bit-installer-%VERSION_NUMBER_CODE%.msi
+
+echo *** creating zip
+rem "Engauge Digitizer" in next line is needed since zip crashes on %RESULTDIR% due to the space
+zip -r "digit-exe-windows-32-bit-without-installer-file-%VERSION_NUMBER_CODE%.zip" "Engauge Digitizer"
