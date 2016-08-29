af931f108170171cbd829defd098c5f3382ec17b
markummitchell <markummitchell@gmail.com>
2016-08-28 23:49:14 -0700

    Main window settings width [ci skip]

--- src/Dlg/DlgSettingsMainWindow.cpp
+++ src/Dlg/DlgSettingsMainWindow.cpp
@@ -28,7 +28,7 @@
 #include "ZoomFactorInitial.h"
 #include "ZoomLabels.h"
 
-const int MINIMUM_DIALOG_WIDTH_MAIN_WINDOW = 530;
+const int MINIMUM_DIALOG_WIDTH_MAIN_WINDOW = 550;
 
 DlgSettingsMainWindow::DlgSettingsMainWindow(MainWindow &mainWindow) :
   DlgSettingsAbstractBase (tr ("Main Window"),
