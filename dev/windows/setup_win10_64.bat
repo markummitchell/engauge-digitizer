9fbbea5159775dc70061e8885a0b7d2acbfea73e
Mark Mitchell <markummitchell@gmail.com>
2016-10-08 19:03:25 -0700

    Windows build scripts use fftw 3.3.4 [ci skip]

--- dev/windows/setup_win10_64.bat
+++ dev/windows/setup_win10_64.bat
@@ -1,12 +1,12 @@
 @echo off
 
 rem Variables must go BEFORE vcvars32.bat which breaks all later SETs
-set QTDIR=C:\Qt\5.6\msvc2013
+set QTDIR=C:\Qt64\5.6\msvc2013_64
 set FFTW_HOME=%USERPROFILE%\fftw-3.3.4
 set LOG4CPP_HOME=%USERPROFILE%\log4cpp_null
 
 rem mingw
-set PATH=/c/MinGW/bin;%QTDIR%/bin;%PATH%
+set PATH=%QTDIR%/bin;%PATH%
 
 rem PATH must point to .../Microsoft Visual Studio 14.0/vc/vcvars32.bat
-vcvars32.bat
+vcvarsall.bat
