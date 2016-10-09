32575a4d4b3c11780c06c9dd94cd9c657f544bbf
markummitchell <markummitchell>
2016-10-09 15:37:33 -0700

    Comment to guide release build in windows [ci skip]

--- dev/windows/build_msi_and_zip.bat
+++ dev/windows/build_msi_and_zip.bat
@@ -92,7 +92,9 @@ rem ps: gc Makefile.orig | %{ $_ -replace '551.lib', %QTLIBEXT% } > Makefile
 if "%ARG1%" neq "norelease" (
   nmake clean
 )
-nmake
+
+rem Make sure the log4cpp library is built with debug info to prevent 'mismatch deteced for _ITERATOR_DEBUG_LEVEL'
+nmake release
 
 if not exist bin/engauge.exe (
   echo "Executable could not be built. Stopping"
