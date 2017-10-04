3c4fa4cdcb47d4eabbd338093c17037778fbfb33
markummitchell <markummitchell@gmail.com>
2017-10-04 09:07:03 -0700

    Make test errors explicit

--- src/Test/TestExport.cpp
+++ src/Test/TestExport.cpp
@@ -213,242 +213,250 @@ void TestExport::initTestCase ()
 
 void TestExport::testCommasInFunctionsForCommasSwitzerland ()
 {
+  QString outputExpected =
+    "x,Curve1\n"
+    """0,001"",""-1,27563""\n"
+    """0,001"",""-1,27563""\n"
+    """0,01"",""-1,26683""\n"
+    """0,1"",""-1,17881""\n"
+    "1,""-0,29658""\n"
+    "10,""-706,15184""\n"
+    "100,""-3997814,14355""\n"
+    "1000,""-4541901224,06376""\n";
+  
   QString outputGot;
   bool success = checkCommasInFunctionsForDelimiter (EXPORT_DELIMITER_COMMA,
                                                      QLocale::Switzerland,
                                                      outputGot);
-  if (success) {
-    QString outputExpected =
-      "x,Curve1\n"
-      """0,001"",""-1,27563""\n"
-      """0,001"",""-1,27563""\n"
-      """0,01"",""-1,26683""\n"
-      """0,1"",""-1,17881""\n"
-      "1,""-0,29658""\n"
-      "10,""-706,15184""\n"
-      "100,""-3997814,14355""\n"
-      "1000,""-4541901224,06376""\n";
-    success = (outputGot == outputExpected);
+  if (!success) {
+    outputGot = "";
   }
 
-  QVERIFY (success);
+  QVERIFY (outputGot == outputExpected);
 }
 
 void TestExport::testCommasInFunctionsForCommasUnitedStates ()
 {
+  QString outputExpected = 
+    "x,Curve1\n"
+    "0.001,-1.27563\n"
+    "0.001,-1.27563\n"
+    "0.01,-1.26683\n"
+    "0.1,-1.17881\n"
+    "1,-0.29658\n"
+    "10,-706.15184\n"
+    "100,-3997814.14355\n"
+    "1000,-4541901224.06376\n";
+  
   QString outputGot;
   bool success = checkCommasInFunctionsForDelimiter (EXPORT_DELIMITER_COMMA,
                                                      QLocale::UnitedStates,                                                     
                                                      outputGot);
-  if (success) {
-    QString outputExpected = 
-      "x,Curve1\n"
-      "0.001,-1.27563\n"
-      "0.001,-1.27563\n"
-      "0.01,-1.26683\n"
-      "0.1,-1.17881\n"
-      "1,-0.29658\n"
-      "10,-706.15184\n"
-      "100,-3997814.14355\n"
-      "1000,-4541901224.06376\n";
-    success = (outputGot == outputExpected);
+  if (!success) {
+    outputGot = "";
   }
 
-  QVERIFY (success);
+  QVERIFY (outputGot == outputExpected);
 }
 
 void TestExport::testCommasInFunctionsForTabsSwitzerland ()
 {
+  QString outputExpected =
+    "x\tCurve1\n"
+    "0,001\t-1,27563\n"
+    "0,001\t-1,27563\n"
+    "0,01\t-1,26683\n"
+    "0,1\t-1,17881\n"
+    "1\t-0,29658\n"
+    "10\t-706,15184\n"
+    "100\t-3997814,14355\n"
+    "1000\t-4541901224,06376\n";
+  
   QString outputGot;
   bool success = checkCommasInFunctionsForDelimiter (EXPORT_DELIMITER_TAB,
                                                      QLocale::Switzerland,
                                                      outputGot);
-  if (success) {
-    QString outputExpected =
-      "x\tCurve1\n"
-      "0,001\t-1,27563\n"
-      "0,001\t-1,27563\n"
-      "0,01\t-1,26683\n"
-      "0,1\t-1,17881\n"
-      "1\t-0,29658\n"
-      "10\t-706,15184\n"
-      "100\t-3997814,14355\n"
-      "1000\t-4541901224,06376\n";
-    success = (outputGot == outputExpected);
+  if (!success) {
+    outputGot = "";
   }
 
-  QVERIFY (success);
+  QVERIFY (outputGot == outputExpected);
 }
 
 void TestExport::testCommasInFunctionsForTabsUnitedStates ()
 {
+  QString outputExpected =
+    "x\tCurve1\n"
+    "0.001\t-1.27563\n"
+    "0.001\t-1.27563\n"
+    "0.01\t-1.26683\n"
+    "0.1\t-1.17881\n"
+    "1\t-0.29658\n"
+    "10\t-706.15184\n"
+    "100\t-3997814.14355\n"
+    "1000\t-4541901224.06376\n";
+  
   QString outputGot;
   bool success = checkCommasInFunctionsForDelimiter (EXPORT_DELIMITER_TAB,
                                                      QLocale::UnitedStates,
                                                      outputGot);
-  if (success) {
-    QString outputExpected =
-      "x\tCurve1\n"
-      "0.001\t-1.27563\n"
-      "0.001\t-1.27563\n"
-      "0.01\t-1.26683\n"
-      "0.1\t-1.17881\n"
-      "1\t-0.29658\n"
-      "10\t-706.15184\n"
-      "100\t-3997814.14355\n"
-      "1000\t-4541901224.06376\n";
-    success = (outputGot == outputExpected);
+  if (!success) {
+    outputGot = "";
   }
 
-  QVERIFY (success);
+  QVERIFY (outputGot == outputExpected);
 }
 
 void TestExport::testCommasInRelationsForCommasSwitzerland ()
 {
+  QString outputExpected =
+    "x,Curve1\n"
+    """3,3192"",""2,08003""\n"
+    """3,3723"",""2,15796""\n"
+    """3,432"",""2,23368""\n"
+    """3,4935"",""2,30883""\n"
+    """3,5539"",""2,38438""\n"
+    """3,6113"",""2,46094""\n"
+    """3,6687"",""2,5375""\n"
+    """3,7261"",""2,61406""\n"
+    """3,7836"",""2,69062""\n"
+    """3,841"",""2,76718""\n"
+    """3,9012"",""2,84276""\n"
+    """3,9628"",""2,91791""\n"
+    """4,0231"",""2,99345""\n"
+    """4,0785"",""3,07067""\n"
+    """4,1339"",""3,14789""\n"
+    """4,1932"",""3,22378""\n"
+    """4,2547"",""3,29893""\n"
+    """4,3156"",""3,37426""\n"
+    """4,3731"",""3,45082""\n"
+    """4,4305"",""3,52738""\n"
+    """4,4892"",""3,60349""\n"
+    """4,5486"",""3,67938""\n";
+  
   QString outputGot;
   bool success = checkCommasInRelationsForDelimiter (EXPORT_DELIMITER_COMMA,
                                                      QLocale::Switzerland,
                                                      outputGot);
-  if (success) {
-    QString outputExpected =
-      "x,Curve1\n"
-      """3,3192"",""2,08003""\n"
-      """3,3723"",""2,15796""\n"
-      """3,432"",""2,23368""\n"
-      """3,4935"",""2,30883""\n"
-      """3,5539"",""2,38438""\n"
-      """3,6113"",""2,46094""\n"
-      """3,6687"",""2,5375""\n"
-      """3,7261"",""2,61406""\n"
-      """3,7836"",""2,69062""\n"
-      """3,841"",""2,76718""\n"
-      """3,9012"",""2,84276""\n"
-      """3,9628"",""2,91791""\n"
-      """4,0231"",""2,99345""\n"
-      """4,0785"",""3,07067""\n"
-      """4,1339"",""3,14789""\n"
-      """4,1932"",""3,22378""\n"
-      """4,2547"",""3,29893""\n"
-      """4,3156"",""3,37426""\n"
-      """4,3731"",""3,45082""\n"
-      """4,4305"",""3,52738""\n"
-      """4,4892"",""3,60349""\n"
-      """4,5486"",""3,67938""\n";
-    success = (outputGot == outputExpected);
+  if (!success) {
+    outputGot = "";
   }
 
-  QVERIFY (success);
+  QVERIFY (outputGot == outputExpected);
 }
 
 void TestExport::testCommasInRelationsForCommasUnitedStates ()
 {
+  QString outputExpected =
+    "x,Curve1\n"
+    "3.3192,2.08003\n"
+    "3.3723,2.15796\n"
+    "3.432,2.23368\n"
+    "3.4935,2.30883\n"
+    "3.5539,2.38438\n"
+    "3.6113,2.46094\n"
+    "3.6687,2.5375\n"
+    "3.7261,2.61406\n"
+    "3.7836,2.69062\n"
+    "3.841,2.76718\n"
+    "3.9012,2.84276\n"
+    "3.9628,2.91791\n"
+    "4.0231,2.99345\n"
+    "4.0785,3.07067\n"
+    "4.1339,3.14789\n"
+    "4.1932,3.22378\n"
+    "4.2547,3.29893\n"
+    "4.3156,3.37426\n"
+    "4.3731,3.45082\n"
+    "4.4305,3.52738\n"
+    "4.4892,3.60349\n"
+    "4.5486,3.67938\n";
+    
   QString outputGot;
   bool success = checkCommasInRelationsForDelimiter (EXPORT_DELIMITER_COMMA,
                                                      QLocale::UnitedStates,
                                                      outputGot);
-  if (success) {
-    QString outputExpected =
-      "x,Curve1\n"
-      "3.3192,2.08003\n"
-      "3.3723,2.15796\n"
-      "3.432,2.23368\n"
-      "3.4935,2.30883\n"
-      "3.5539,2.38438\n"
-      "3.6113,2.46094\n"
-      "3.6687,2.5375\n"
-      "3.7261,2.61406\n"
-      "3.7836,2.69062\n"
-      "3.841,2.76718\n"
-      "3.9012,2.84276\n"
-      "3.9628,2.91791\n"
-      "4.0231,2.99345\n"
-      "4.0785,3.07067\n"
-      "4.1339,3.14789\n"
-      "4.1932,3.22378\n"
-      "4.2547,3.29893\n"
-      "4.3156,3.37426\n"
-      "4.3731,3.45082\n"
-      "4.4305,3.52738\n"
-      "4.4892,3.60349\n"
-      "4.5486,3.67938\n";
-    success = (outputGot == outputExpected);
+  if (!success) {
+    outputGot = "";
   }
 
-  QVERIFY (success);
+  QVERIFY (outputGot == outputExpected);
 }
 
 void TestExport::testCommasInRelationsForTabsSwitzerland ()
 {
+  QString outputExpected =
+    "x\tCurve1\n"
+    "3,3192\t2,08003\n"
+    "3,3723\t2,15796\n"
+    "3,432\t2,23368\n"
+    "3,4935\t2,30883\n"
+    "3,5539\t2,38438\n"
+    "3,6113\t2,46094\n"
+    "3,6687\t2,5375\n"
+    "3,7261\t2,61406\n"
+    "3,7836\t2,69062\n"
+    "3,841\t2,76718\n"
+    "3,9012\t2,84276\n"
+    "3,9628\t2,91791\n"
+    "4,0231\t2,99345\n"
+    "4,0785\t3,07067\n"
+    "4,1339\t3,14789\n"
+    "4,1932\t3,22378\n"
+    "4,2547\t3,29893\n"
+    "4,3156\t3,37426\n"
+    "4,3731\t3,45082\n"
+    "4,4305\t3,52738\n"
+    "4,4892\t3,60349\n"
+    "4,5486\t3,67938\n";
+  
   QString outputGot;
   bool success = checkCommasInRelationsForDelimiter (EXPORT_DELIMITER_TAB,
                                                      QLocale::Switzerland,
                                                      outputGot);
-  if (success) {
-    QString outputExpected =
-      "x\tCurve1\n"
-      "3,3192\t2,08003\n"
-      "3,3723\t2,15796\n"
-      "3,432\t2,23368\n"
-      "3,4935\t2,30883\n"
-      "3,5539\t2,38438\n"
-      "3,6113\t2,46094\n"
-      "3,6687\t2,5375\n"
-      "3,7261\t2,61406\n"
-      "3,7836\t2,69062\n"
-      "3,841\t2,76718\n"
-      "3,9012\t2,84276\n"
-      "3,9628\t2,91791\n"
-      "4,0231\t2,99345\n"
-      "4,0785\t3,07067\n"
-      "4,1339\t3,14789\n"
-      "4,1932\t3,22378\n"
-      "4,2547\t3,29893\n"
-      "4,3156\t3,37426\n"
-      "4,3731\t3,45082\n"
-      "4,4305\t3,52738\n"
-      "4,4892\t3,60349\n"
-      "4,5486\t3,67938\n";
-    success = (outputGot == outputExpected);
+  if (!success) {
+    outputGot = "";
   }
 
-  QVERIFY (success);
+  QVERIFY (outputGot == outputExpected);
 }
 
 void TestExport::testCommasInRelationsForTabsUnitedStates ()
 {
+  QString outputExpected =
+    "x\tCurve1\n"
+    "3.3192\t2.08003\n"
+    "3.3723\t2.15796\n"
+    "3.432\t2.23368\n"
+    "3.4935\t2.30883\n"
+    "3.5539\t2.38438\n"
+    "3.6113\t2.46094\n"
+    "3.6687\t2.5375\n"
+    "3.7261\t2.61406\n"
+    "3.7836\t2.69062\n"
+    "3.841\t2.76718\n"
+    "3.9012\t2.84276\n"
+    "3.9628\t2.91791\n"
+    "4.0231\t2.99345\n"
+    "4.0785\t3.07067\n"
+    "4.1339\t3.14789\n"
+    "4.1932\t3.22378\n"
+    "4.2547\t3.29893\n"
+    "4.3156\t3.37426\n"
+    "4.3731\t3.45082\n"
+    "4.4305\t3.52738\n"
+    "4.4892\t3.60349\n"
+    "4.5486\t3.67938\n";
+  
   QString outputGot;
   bool success = checkCommasInRelationsForDelimiter (EXPORT_DELIMITER_TAB,
                                                      QLocale::UnitedStates,
                                                      outputGot);
-  if (success) {
-    QString outputExpected =
-      "x\tCurve1\n"
-      "3.3192\t2.08003\n"
-      "3.3723\t2.15796\n"
-      "3.432\t2.23368\n"
-      "3.4935\t2.30883\n"
-      "3.5539\t2.38438\n"
-      "3.6113\t2.46094\n"
-      "3.6687\t2.5375\n"
-      "3.7261\t2.61406\n"
-      "3.7836\t2.69062\n"
-      "3.841\t2.76718\n"
-      "3.9012\t2.84276\n"
-      "3.9628\t2.91791\n"
-      "4.0231\t2.99345\n"
-      "4.0785\t3.07067\n"
-      "4.1339\t3.14789\n"
-      "4.1932\t3.22378\n"
-      "4.2547\t3.29893\n"
-      "4.3156\t3.37426\n"
-      "4.3731\t3.45082\n"
-      "4.4305\t3.52738\n"
-      "4.4892\t3.60349\n"
-      "4.5486\t3.67938\n";
-    success = (outputGot == outputExpected);
+  if (!success) {
+    outputGot = "";
   }
 
-  QVERIFY (success);
+  QVERIFY (outputGot == outputExpected);
 }
 
 void TestExport::testLogExtrapolationFunctionsAll ()
